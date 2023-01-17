58141d6d58c5fc1730b65009fc111188978bedc0
Chester Vincent <chetvincent@chesters-mbp-2.mynetworksettings.com>
2023-01-17 11:38:11 -0500

    java project initialized

---
+++ java_coffeeshop_backend/src/main/java/com/coffeeshop/java_coffeeshop_backend/JavaCoffeeshopBackendApplication.java
@@ -0,0 +1,13 @@
+package com.coffeeshop.java_coffeeshop_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCoffeeshopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCoffeeshopBackendApplication.class, args);
+	}
+
+}
---
+++ java_coffeeshop_backend/src/test/java/com/coffeeshop/java_coffeeshop_backend/JavaCoffeeshopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffeeshop.java_coffeeshop_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCoffeeshopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
